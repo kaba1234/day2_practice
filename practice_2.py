d5b2d09c8a25981a1f9fe97512fb4c5f3e7bb385
KABA JUNIOR <kabajunior24@gmail.com>
2024-11-23 12:57:18 +0000

    update

---
+++ practice_2.py
@@ -0,0 +1,30 @@
+age =25
+year = 2024
+print(age, year)
+
+tempreture = 36.5
+pi = 3.14159
+print=(tempreture,pi)
+
+z = 2+3j
+print(z.real, z.imag)
+
+is_active = true
+is_verified = false
+print(is_active, is_verified)
+
+greeting ="Hello, world!"
+name ='Alice'
+print(greeting, name)
+
+Fruits =["apple", "banana", cherry"]
+
+coordinates = (10, 20, 30)
+colors = ("red", "green", "blue")
+
+range_numbers = range (1, 10)
+print(list(range_numbers))
+
+
+
+
